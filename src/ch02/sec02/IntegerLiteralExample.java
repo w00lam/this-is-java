2c35b95a723fb39d21955da61e0e52f60ff5878a
이우람 <wlam135@naver.com>
2025-05-24 15:07:37 +0900

    IntegerLiteralExample

---
+++ src/ch02/sec02/IntegerLiteralExample.java
@@ -0,0 +1,15 @@
+package ch02.sec02;
+
+public class IntegerLiteralExample {
+    public static void main(String[] args) {
+        int va1 = 0b1011;   //2진수
+        int va2 = 0206;     //8진수
+        int va3 = 365;      //10진수
+        int va4 = 0xB3;     //16진수
+
+        System.out.println("var1: " + va1);
+        System.out.println("var2: " + va2);
+        System.out.println("var3: " + va3);
+        System.out.println("var4: " + va4);
+    }
+}
