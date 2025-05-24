ab95986a91e250451db12a801c59eb33bbd17ba7
이우람 <wlam135@naver.com>
2025-05-24 15:15:37 +0900

    ByteExample

---
+++ src/ch02/sec02/ByteExample.java
@@ -0,0 +1,20 @@
+package ch02.sec02;
+
+public class ByteExample {
+    public static void main(String[] args) {
+        byte var1 = -128;
+        byte var2 = -30;
+        byte var3 = 0;
+        byte var4 = 30;
+        byte var5 = 127;
+        //byte var6 = 128; 컴파일 에러(Type mismatch: cannot convert from int to byte)
+        //저장되는 값의 허용 범위: -2^7 ~ (2^7-1)
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+        //System.out.println(var6);
+    }
+}
