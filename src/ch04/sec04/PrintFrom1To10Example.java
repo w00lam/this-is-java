e31953efad2ccad636bdeabccf7e9412c5abdc55
wlam135 <wlam135@naver.com>
2025-05-28 21:26:45 +0900

    PrintFrom1To10Example

--- src/ch04/sec04/PrintFrom1To10Example.java
+++ src/ch04/sec04/PrintFrom1To10Example.java
@@ -3,7 +3,7 @@ package ch04.sec04;
 public class PrintFrom1To10Example {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i + " ");
+            System.out.print(i + " ");
         }
     }
 }
