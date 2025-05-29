7d831a8905701cf07ed571c1840801860b8bf074
wlam135 <wlam135@naver.com>
2025-05-29 23:23:45 +0900

    ArrayCreateByValueListExample1

--- src/ch05/sec06/ArrayCreateByValueListExample1.java
+++ src/ch05/sec06/ArrayCreateByValueListExample1.java
@@ -1,6 +1,6 @@
 package ch05.sec06;
 
-public class ArrayCreateByValueListExample {
+public class ArrayCreateByValueListExample1 {
     public static void main(String[] args) {
         //배열 변수 선언과 배열 생성
         String[] season = {"Spring", "Summer", "Fall", "Winter"};
