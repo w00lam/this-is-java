0a3a36940207ecf0be92da9d60516222169b61b4
wlam135 <wlam135@naver.com>
2025-06-02 23:22:31 +0900

    Exam02

--- src/ch06/sec13/exam02/package1/B.java
+++ src/ch06/sec13/exam02/package1/B.java
@@ -2,7 +2,9 @@ package ch06.sec13.exam02.package1; //<----------패키지가 동일
 
 public class B {
     //필드 선언
-    A a1 = new A(true);       //o
-    A a2 = new A(1);          //o
-    //A a3 = new A("문자열");     //x <----------private 생성자 접근 불가(컴파일 에러)
+    A a1 = new A(true);    //o
+    A a2 = new A(1);       //o
+    /*
+    A a3 = new A("문자열");    //x <----------private 생성자 접근 불가(컴파일 에러)
+     */
 }
