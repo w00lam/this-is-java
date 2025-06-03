8234e13393a38d84e96f2b8bc9c44d0fa627912b
wlam135 <wlam135@naver.com>
2025-06-03 22:50:59 +0900

    SmartPhoneExample

--- src/ch07/sec02/SmartPhoneExample.java
+++ src/ch07/sec02/SmartPhoneExample.java
@@ -3,7 +3,7 @@ package ch07.sec02;
 public class SmartPhoneExample {
     public static void main(String[] args) {
         //SmartPhone 객체 생성
-        SmartPhone myPhone = new SmartPhone("갤럭시25", "화이트블루");
+        SmartPhone myPhone = new SmartPhone("갤럭시25+", "화이트블루");
 
         //Phone으로부터 상속받은 필드 읽기
         System.out.println("모델: " + myPhone.model);
