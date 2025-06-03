7a8707526dbdf30be07a6e835d64dd9e1914e4e3
wlam135 <wlam135@naver.com>
2025-06-03 22:05:23 +0900

    Singleton

--- src/ch06/sec15/Singleton.java
+++ src/ch06/sec15/Singleton.java
@@ -1,7 +1,7 @@
 package ch06.sec15;
 
 public class Singleton {
-    //private 접근 권한ㅇ르 갖는 정적 필드 선언과 초기화
+    //private 접근 권한을 갖는 정적 필드 선언과 초기화
     private static Singleton singleton = new Singleton();
 
     //private 접근 권한을 갖는 생성자 선언
