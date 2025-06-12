65d9b9ede053c186283ac60d721ad11b9f47d8b4
w00lam <98564214+w00lam@users.noreply.github.com>
2025-06-12 22:57:25 +0900

    CalendarExample

---
+++ src/ch12/sec08/CalendarExample.java
@@ -0,0 +1,42 @@
+package ch12.sec08;
+
+import java.util.Calendar;
+
+public class CalendarExample {
+    public static void main(String[] args) {
+        Calendar now = Calendar.getInstance();
+
+        int year = now.get(Calendar.YEAR);
+        int month = now.get(Calendar.MONTH);
+        int day = now.get(Calendar.DAY_OF_MONTH);
+        int week = now.get(Calendar.DAY_OF_WEEK);
+        String strWeek = switch (week) {
+            case Calendar.MONDAY -> "월";
+            case Calendar.TUESDAY -> "화";
+            case Calendar.WEDNESDAY -> "수";
+            case Calendar.THURSDAY -> "목";
+            case Calendar.FRIDAY -> "금";
+            case Calendar.SATURDAY -> "토";
+            default -> "일";
+        };
+
+        int amPm = now.get(Calendar.AM_PM);
+        String strAmPm = switch (amPm) {
+            case Calendar.AM -> "오전";
+            default -> "오후";
+        };
+
+        int hour = now.get(Calendar.HOUR);
+        int minute = now.get(Calendar.MINUTE);
+        int second = now.get(Calendar.SECOND);
+
+        System.out.print(year + "년 ");
+        System.out.print(month + "월 ");
+        System.out.println(day + "일 ");
+        System.out.print(strWeek + "요일 ");
+        System.out.println(strAmPm + " ");
+        System.out.print(hour + "시 ");
+        System.out.print(minute + "분 ");
+        System.out.println(second + "초 ");
+    }
+}
