084cad159535253d8f48b7333f1a0e063b57cb45
w00lam <98564214+w00lam@users.noreply.github.com>
2025-06-18 22:22:16 +0900

    LambdaExample

---
+++ src/ch16/sec03/Person.java
@@ -0,0 +1,11 @@
+package ch16.sec03;
+
+public class Person {
+    public void action1(Workable workable) {
+        workable.work("구덕팔", "프로그래밍");
+    }
+
+    public void action2(Speackable speackable) {
+        speackable.speak("안녕하세요");
+    }
+}
---
+++ src/ch16/sec03/Speackable.java
@@ -0,0 +1,6 @@
+package ch16.sec03;
+
+@FunctionalInterface
+public interface Speackable {
+    void speak(String content);
+}
