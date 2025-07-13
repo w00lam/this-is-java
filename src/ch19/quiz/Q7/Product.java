70f0bf616ff1f20424d7a936d6035efac8364556
w00lam <98564214+w00lam@users.noreply.github.com>
2025-07-13 20:37:51 +0900

    Q7

--- src/ch19/quiz/Q7/Product.java
+++ src/ch19/quiz/Q7/Product.java
@@ -1,46 +1,15 @@
 package ch19.quiz.Q7;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     private int no;
     private String name;
     private int price;
     private int stock;
-
-    public Product() {
-    }
-
-    public Product(int no, String name, int price, int stock) {
-        this.no = no;
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-    }
-
-    public int getNo() {
-        return no;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
 }
